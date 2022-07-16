c15147aa2e6068b371f543f6b83e967271e46d7e
MARIA IVANOVA <mashasedaya199317@gmail.com>
2022-07-16 12:31:05 +0300

    add calorieInTake

---
+++ src/redux/dailyCalorieIntakes/dailyCalorieIntake-slice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import dailyCalories from './dailyCalorieIntake-operations';
+
+const initialState = {
+  calories: null,
+  notRecommendProd: [],
+};
+
+const caloriesSlice = createSlice({
+  name: 'calories',
+  initialState,
+  extraReducers: {
+    [dailyCalories.fulfilled](state, action) {
+      state.calories = action.payload.data.user.dailyCalorieIntake;
+      state.notRecommendProd = [
+        ...action.payload.data.user.notRecommendedProducts,
+      ];
+      // console.log(action);
+      console.log(state);
+    },
+  },
+});
+
+export default caloriesSlice.reducer;
