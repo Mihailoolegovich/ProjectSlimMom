2c32f66e941ee9abf1af3243faddb4444012b796
MARIA IVANOVA <mashasedaya199317@gmail.com>
2022-07-18 00:18:48 +0300

    add markup and styles

---
+++ src/components/DailyCaloriesIntake/DailyCaloriesIntake.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import s from './DailyCaloriesIntake.module.scss';
+import { useSelector } from 'react-redux';
+import {
+  getCalories,
+  getNotRecommendProd,
+} from '../../redux/dailyCalorieIntakes/dailyCalorieIntake-selectors';
+
+const DailyCaloriesIntake = () => {
+  const calories = useSelector(getCalories);
+  const notRecommendProduct = useSelector(getNotRecommendProd);
+  console.log(calories);
+  console.log(notRecommendProduct);
+
+  return (
+    <div className={s.mainContainer}>
+      <div className={s.container}>
+        <h3 className={s.title}>Your recommended daily calorie intake is</h3>
+        <div className={s.value}>
+          <span className={s.calories}>{calories}</span>
+          <span className={s.text}> kcal</span>
+        </div>
+      </div>
+
+      <div className={s.containerFood}>
+        <h3 className={s.productsTitle}>Foods you should not eat</h3>
+        <ol className={s.productsList}>
+          {notRecommendProduct.map((product, id) => (
+            <li key={id} className={s.productItem}>
+              {product.en}
+            </li>
+          ))}
+        </ol>
+      </div>
+
+      <Link type="button" to="/auth/login" className={s.button}>
+        Start losing weight
+      </Link>
+    </div>
+  );
+};
+export default DailyCaloriesIntake;
